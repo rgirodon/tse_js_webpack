47ecdcdd011ab1f9ba1c9284cc1f166b6577a5b2
rgirodon <remy.girodon@gmail.com>
2020-09-28 15:02:03 +0200

    Step 1

---
+++ src/index.js
@@ -0,0 +1,11 @@
+function buildHelloWorldDiv() {
+
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'World', '!'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(buildHelloWorldDiv());
